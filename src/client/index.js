2e987404b498cfcd8301f2763c8a547ca19e1267
charis <charis@macbookpro-0638.fritz.box>
2020-06-23 18:25:54 +0200

    fix export problems

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,4 @@
 import { addTrip } from "./js/app.js"
-import { newElement } from "./js/app.js"
-import { invalidCityName } from "./js/cityNameValidator.js"
 import { addTripEvList } from "./js/app.js"
 import { getCityInfo} from "./js/api/geonames.js"
 import { getWeather} from "./js/api/geonames.js"
@@ -8,9 +6,6 @@ import { getWeather} from "./js/api/geonames.js"
 import './styles/main.scss'
 
 export { addTrip }
-export { invalidCityName }
 export { addTripEvList }
-export { newElement }
 export{getCityInfo}
 export{getWeather}
-export {invalidCityName}
